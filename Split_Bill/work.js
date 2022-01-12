f5fcdd283f16afa49aa60fa52c3d6c310d3a2e61
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-01-12 17:25:36 +0530

    allSET

--- Split_Bill/work.js
+++ Split_Bill/work.js
@@ -6,14 +6,32 @@ function billDividerHandler(event) {
     event.preventDefault();
     const inputs = getInputs();
     console.log(inputs);
+    showCalculateOutput(inputs);
 }
 
 function getInputs() {
     let subTotal = document.getElementById('subtotal').value;
     let tipPercent = document.getElementById('tip').value;
-    let numPeople = document.getElementById('no-of-person').value;
-    // console.log(subTotal, tipPercent, numPeople);
-    return { subTotal, tipPercent, numPeople };
+    let noOfPerson = document.getElementById('no-of-person').value;
+    // console.log(subTotal, tipPercent, noOfPerson);
+    return { subTotal, tipPercent, noOfPerson };
+
+
+}
+
+function showCalculateOutput(input) {
+    billDivideForm.reset();
+
+    let totalTip = (parseFloat(input.subTotal) * parseFloat(input.tipPercent)) / 100;
+
+    let totalAmount = parseFloat(input.subTotal) + totalTip;
+    let tipPerPerson = totalTip / parseFloat(input.noOfPerson);
+    let amountPerPerson = totalAmount / parseFloat(input.noOfPerson);
+
+    document.getElementById('total-bill').innerHTML = `$ ${totalAmount.toFixed(2)}`;
+    document.getElementById('bill-per-person').innerHTML = `$ ${amountPerPerson.toFixed(2)}`;
+    document.getElementById('total-tip').innerHTML = `$ ${totalTip.toFixed(2)}`;
+    document.getElementById('tip-per-person').innerHTML = `$ ${tipPerPerson.toFixed(2)}`;
 
 
 }
